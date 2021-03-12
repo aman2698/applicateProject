a659a06a6f4fb488c6fadbcce9a6421d9ad8d7c0
Aman Sikarwar <71030460+aman2698@users.noreply.github.com>
2021-03-12 18:37:29 +0530

    Add files via upload

---
+++ src/Components/data.js
@@ -0,0 +1,124 @@
+export const order = [
+  {
+    orderNo: "1232423",
+    totalAmount: 34243,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 23424,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 323434,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 343234,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 3434,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 342423,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+  {
+    orderNo: "1232423",
+    totalAmount: 2324,
+    totalQuantity: "232",
+    userName: "aman",
+  },
+];
+
+export const user = [
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+  {
+    userName: "wdxwx",
+    totalAmount: "13232",
+    totalQuantity: "wxwxwed",
+    city: "xwedx",
+  },
+];
+ 
+export const data ={
+    todayOrder:200,
+    currentWeekOrder:450,
+    todayOrderAmount:20000,
+    currentWeekAmount:45000,
+    MTDorder:400,
+    lastMonthOrder:200,
+    MTDorderAmount:65000,
+    lastMonthAmount:45000
+}
+export const chart=[
+    {
+        x: "7/1/2020",
+        y: 20000
+    },
+    {
+        x: "7/2/2020",
+        y: 30000
+    },
+    {
+        x: "7/3/2020",
+        y: 22000
+    },
+    {
+        x: "7/4/2020",
+        y: 45000
+    },
+]
